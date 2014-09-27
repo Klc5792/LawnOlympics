9f6c05917fc9f64ece063d141e7e22a2e9564948
Klc5792 <katcoo1955@yahoo.com>
2014-09-27 03:53:19 -0400

    fixed ors and comments
    fixed ifs to be e or events, o or olympians, and h or help and the
    comments that go with them

--- src/LawnOlympics/LawnOlympics.java
+++ src/LawnOlympics/LawnOlympics.java
@@ -6,55 +6,57 @@ package LawnOlympics;
 class LawnOlympics {
 
     public static void main(String[] args) {
-                // printing a blank line here between the C:\Program and the Title of the Program
+                    // printing a blank line here between the C:\Program and the Title of the Program
         System.out.println(" ");
-                // prints the title of my program
+                    // prints the title of my program
         System.out.println("Lawn Olympics Games");
-                // I wanted another blank line here
+                    // I wanted another blank line here
         System.out.println(" ");
-                // prints a little help for the user
-        System.out.println("For help, type h and enter.");
-                // Just another blank line
+                    // prints a little help for the user
+        System.out.println("For help, type h or help and enter.");
+                    // Just another blank line
         System.out.println(" ");
 
         if (args.length < 1)
-                System.out.println("You must enter something. Please try again or go to help.");
-        else if (args[0].equals("e"))
-                //calls the eventlist method if e is entered
+            System.out.print("You must enter something. Please try again.");
+        else if ((args[0].equals("e")) || (args[0].equals("events")))
+                     //calls the eventlist method if e or events is entered
             eventList();
-        else if (args[0].equals("o"))
-                // calls the olympianList method if o is entered
+        else if ((args[0].equals("o"))   || (args[0].equals("olympians")))
+                    //calls the olympianList method if o or olympians is entered
             olympianList();
-        else if (args[0].equals("h"))
-                //calls the helpList method if h is entered
+        else if ((args[0].equals("h"))  ||  (args[0].equals("help")))
+                    //calls the helpList method if h or help is entered
             helpList();
         else
-                // prints error message if not e, o or h
-            System.out.println("Please enter e, o, or h and enter");
+                     // prints error message if not e, o or h
+            System.out.println("Please enter e or events for a list of events, " +
+                    "or olympians for a list of olympians, or h or help for assistance and enter");
     }
 
-                // helpList method defined
+                    // helpList method defined
         public static void helpList() {
-                // initializes and populates an array of help messages
+                    // initializes and populates an array of help messages
             String[] helpArray =
-                    {"for a list of events, type e", "for a list of olympians type o"};
+                    {"for a list of events, type e or events",
+                     "for a list of olympians type o or olympians"};
             for (int i = 0; i < helpArray.length; i++)
-                // prints the array of help messages
+                    // prints the array of help messages
                 System.out.println(helpArray[i]);
         }
 
-                // eventList method defined
+                    // eventList method defined
         public static void eventList() {
-                // initializes and populates an array of events
+            // initializes and populates an array of events
             String[] eventArray = {"Washoos ", "CanJam ", "Horseshoes ", "Cornhole ", "Ladderball ", "Stickgame "};
             for (int i = 0; i < eventArray.length; i++)
-                // prints the array of events
+                    //prints the array of events
                 System.out.println(eventArray[i]);
         }
 
-                // olympianList method defined
-        public static void olympianList() {
-                // initializes and populates a 2 dimensional array
+                    // olympianList method defined
+    public static void olympianList() {
+                    // initializes and populates a 2 dimensional array
         String[][] olympianArray = {
                 {"John Doe, ", "M, ", "45"},
                 {"Terri Doe, ", "F, ", "43"},
@@ -73,8 +75,7 @@ class LawnOlympics {
                 {"Margaret Wheeler, ", "F, ", "25"},
                 {"Ardie Wheeler, ", "M, ", "30"}
         };
-
-                // prints the 2D array
+                    // prints the 2D array
         for(int i = 0; i < olympianArray.length; i++) {
             for (int j = 0; j < olympianArray[i].length; j++) {
                 System.out.print(olympianArray[i][j]);
